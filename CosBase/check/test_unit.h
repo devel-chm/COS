397533f885e8f145d925c84193021e68380fe83a
Chris Marshall <devel.chm.01@gmail.com>
2018-03-28 15:57:06 -0400

    Create single file unit tests for each COS_PP_ command

---
+++ CosBase/check/test_unit.h
@@ -0,0 +1,27 @@
+/* This is a sample input for testing cpp macros.
+ *
+ *  Run with the following command:
+ * 
+ *    ucpp.exe -I<path to CosBase/include> -l test.in | grep -v '^ *$'  
+ * 
+ *  where -l skips #line output, -C includes comments in the
+ *  output, and the grep command removes lots of whitespace
+ *  lines in the output.  I would like to run this from perl
+ *  so the grep could be done at the perl level.  Some issues
+ *  to work out:
+ * 
+ *  - Would like to show input and output
+ *  - File and routine completion (incremental?)
+ *  - Call ucpp as a lexer directly with XS
+ *  - Implement tests with error outputs
+ */
+
+#include "cos/cpp/cpp.h"  // This must be adapted for actual locations
+
+COS_PP_1ST((a,b));
+
+COS_PP_1ST(());
+
+COS_PP_1ST(a);  // Doesn't handle non-tuple argument
+
+COS_PP_1ST();  // Doesn't handle missing tuple argument
